c52c20497718d169babc3f0ed7305e8c390e00a3
dannybouwers <d.bouwers+github@gmail.com>
2022-03-04 22:09:00 +0100

    Removed TransIP

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@ touch -a ${PWD}/data/traefik/traefik.log
 touch -a ${PWD}/data/traefik/acme.json
 chmod 600 ${PWD}/data/traefik/acme.json
 
-touch -a ${PWD}/data/traefik/acme-cf.json
-chmod 600 ${PWD}/data/traefik/acme-cf.json
-
 mkdir -p ${PWD}/data/unifi/config
 
 mkdir -p ${PWD}/data/adguard/work
