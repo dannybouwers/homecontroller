457eeb912045be67b8858ab38c231e45a128a703
dannybouwers <d.bouwers+github@gmail.com>
2023-06-04 21:46:09 +0200

    Cleaned Traefik in Compose

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/sh
 mkdir -p ${PWD}/data/traefik
-touch -a ${PWD}/data/traefik/traefik.log
-touch -a ${PWD}/data/traefik/acme.json
-chmod 600 ${PWD}/data/traefik/acme.json
 
 mkdir -p ${PWD}/data/unifi/config
 
