81465bf740c4b69b8ee0f859cc3a472eb5e697bb
dannybouwers <d.bouwers+github@gmail.com>
2024-06-25 21:12:11 +0000

    Auto-commit from Azure DevOps

--- tests/firefly-importer.sh
+++ tests/firefly-importer.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 . ./tests
 
+sleep 10
 check_docker_container "fireflyiii/data-importer"
+
+sleep 60
 check_url_status "https://firefly-importer.$1/health"
--- tests/firefly.sh
+++ tests/firefly.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 . ./tests
 
+sleep 10
 check_docker_container "fireflyiii/core"
+
+sleep 60
 check_url_status "https://firefly.$1/health"
